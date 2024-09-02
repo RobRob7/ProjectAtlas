7ead8a276f3a48b690423c96fb223a8b366c8df8
RobRob7 <rjames11@nmsu.edu>
2024-09-02 15:34:13 -0600

    minor changes to file names and remove some leftover code

--- OWiseOpenGL/main.cpp
+++ OWiseOpenGL/main.cpp
@@ -1,6 +1,6 @@
 #include <glad/glad.h>
 #include <GLFW/glfw3.h>
-#include "Shader.h"
+#include "Shader.hpp"
 
 #include <iostream>
 
@@ -9,22 +9,6 @@ void framebuffer_size_callback(GLFWwindow* window, int width, int height);
 
 // is called when certain input action observed
 void processInput(GLFWwindow* window);
-
-const char* vertexShaderSource = 
-	"#version 330 core\n" 
-	"layout(location=0) in vec3 aPos;\n"
-	"void main()\n"
-	"{\n"
-	"gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);\n"
-	"}\0";
-
-const char* fragmentShaderSource =
-"#version 330 core\n"
-"out vec4 FragColor;\n"
-"void main()\n"
-"{\n"
-"FragColor = vec4(1.0f, 0.5f, 0.2f, 1.0f);\n"
-"}\0";
 
 int main()
 {
