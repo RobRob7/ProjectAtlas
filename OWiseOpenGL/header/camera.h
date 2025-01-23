31faaafa6d8b148afcc028d5dca3edf0b14c42de
RobRob7 <rjames11@nmsu.edu>
2025-01-22 19:25:22 -0700

    Ch 10: Camera (done)

---
+++ OWiseOpenGL/header/camera.h
@@ -0,0 +1,136 @@
+#ifndef CAMERA_H
+#define CAMERA_H
+
+#include <glad/glad.h>
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+
+// camera movement options
+enum Camera_Movmement {
+	FORWARD,
+	BACKWARD,
+	LEFT,
+	RIGHT
+};
+
+// default camera values
+const float YAW			= -90.0f;
+const float PITCH		= 0.0f;
+const float SPEED		= 2.5f;
+const float SENSITIVITY = 0.1f;
+const float ZOOM		= 45.0f;
+
+// camera class
+class Camera
+{
+public:
+	// camera attributes
+	glm::vec3 Position;
+	glm::vec3 Front;
+	glm::vec3 Up;
+	glm::vec3 Right;
+	glm::vec3 WorldUp;
+
+	// euler angles
+	float Yaw;
+	float Pitch;
+
+	// camera options
+	float MovementSpeed;
+	float MouseSensitivity;
+	float Zoom;
+
+	// constructor with vectors
+	Camera(glm::vec3 position = glm::vec3(0.0f, 0.0f, 0.0f), glm::vec3 up = glm::vec3(0.0f, 1.0f, 0.0f), float yaw = YAW, float pitch = PITCH) : Front(glm::vec3(0.0f, 0.0f, -1.0f)), MovementSpeed(SPEED), MouseSensitivity(SENSITIVITY), Zoom(ZOOM)
+	{
+		Position = position;
+		WorldUp = up;
+		Yaw = yaw;
+		Pitch = pitch;
+		updateCameraVectors();
+	} // end constructor
+
+	// constructor with scalar values
+	Camera(float posX, float posY, float posZ, float upX, float upY, float upZ, float yaw, float pitch) : Front(glm::vec3(0.0f, 0.0f, -1.0f)), MovementSpeed(SPEED), MouseSensitivity(SENSITIVITY), Zoom(ZOOM)
+	{
+		Position = glm::vec3(posX, posY, posZ);
+		WorldUp = glm::vec3(upX, upY, upZ);
+		Yaw = yaw;
+		Pitch = pitch;
+		updateCameraVectors();
+	} // end constructor
+
+	// returns the view matrix calculated using Euler angles and LookAt matrix
+	glm::mat4 GetViewMatrix()
+	{
+		return glm::lookAt(Position, Position + Front, Up);
+	} // end of GetViewMatrix()
+
+	// processes input received from any keyboard-like input system.
+	// accepts input parameter in the form of camera defined ENUM
+	void ProcessKeyboard(Camera_Movmement direction, float deltaTime)
+	{
+		float velocity = MovementSpeed * deltaTime;
+		if (direction == FORWARD)
+			Position += Front * velocity;
+		if (direction == BACKWARD)
+			Position -= Front * velocity;
+		if (direction == LEFT)
+			Position -= Right * velocity;
+		if (direction == RIGHT)
+			Position += Right * velocity;
+	} // end of ProcessKeyboard()
+
+	// processes input received from a mouse input system. expects the offset value
+	// in both the x and y direction
+	void ProcessMouseMovement(float xoffset, float yoffset, GLboolean constrainPitch = true)
+	{
+		xoffset *= MouseSensitivity;
+		yoffset *= MouseSensitivity;
+
+		Yaw	  += xoffset;
+		Pitch += yoffset;
+
+		// check pitch within bounds
+		if (constrainPitch)
+		{
+			if (Pitch > 89.0f)
+				Pitch = 89.0f;
+			if (Pitch < -89.0f)
+				Pitch = -89.0f;
+		}
+
+		// update Front, Right, and Up vectors using updated Euler angles
+		updateCameraVectors();
+	} // end of ProcessMouseMovement()
+
+	// processes input received from a mouse scroll-wheel event. only requires
+	// input on the vertical wheel-axis
+	void ProcessMouseScroll(float yoffset)
+	{
+		Zoom -= (float)yoffset;
+		if (Zoom < 1.0f)
+			Zoom = 1.0f;
+		if (Zoom > 45.0f)
+			Zoom = 45.0f;
+	} // end of ProcessMouseScroll()
+
+private:
+	// calculates the front vector from the Camera's (updated) Euler angles
+	void updateCameraVectors()
+	{
+		// calculate the new Front vector
+		glm::vec3 front;
+		front.x = cos(glm::radians(Yaw)) * cos(glm::radians(Pitch));
+		front.y = sin(glm::radians(Pitch));
+		front.z = sin(glm::radians(Yaw)) * cos(glm::radians(Pitch));
+		Front = glm::normalize(front);
+
+		// re-calculate Right and Up vector
+		Right = glm::normalize(glm::cross(Front, WorldUp));
+		Up = glm::normalize(glm::cross(Right, Front));
+	} // end of updateCameraVectors()
+};
+
+
+#endif
--- OWiseOpenGL/src/main.cpp
+++ OWiseOpenGL/src/main.cpp
@@ -5,15 +5,38 @@
 #include <glm/gtc/type_ptr.hpp>
 #include "../header/shader.hpp"
 #include "../header/stb_image.h"
+#include "../header/camera.h"
 
 #include <iostream>
 
 // is called each time window is resized
 void framebuffer_size_callback(GLFWwindow* window, int width, int height);
 
+// is called when mouse cursor input changes
+void mouse_callback(GLFWwindow* window, double xpos, double ypos);
+
+// is called when scroll-wheel input changes
+void scroll_callback(GLFWwindow* window, double xoffset, double yoffset);
+
 // is called when certain input action observed
 void processInput(GLFWwindow* window);
 
+// window size settings
+const unsigned int SCR_WIDTH = 800;
+const unsigned int SCR_HEIGHT = 600;
+
+// camera
+Camera camera(glm::vec3(0.0f, 0.0f, 3.0f));
+float lastX = SCR_WIDTH / 2.0f;
+float lastY = SCR_HEIGHT / 2.0f;
+bool firstMouse = true;
+
+// time between current frame and last frame
+float deltaTime = 0.0f;
+// time of last frame
+float lastFrame = 0.0f;
+
+// main driver code
 int main()
 {
 	// intialize GLFW
@@ -37,8 +60,13 @@ int main()
 	// tell GLFW to make the context of our window the main context on the current thread
 	glfwMakeContextCurrent(window);
 
-	// tell GLFW to call function on every window resize
+	// callback functions
 	glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
+	glfwSetCursorPosCallback(window, mouse_callback);
+	glfwSetScrollCallback(window, scroll_callback);
+
+	// tell GLFW to capture our mouse
+	glfwSetInputMode(window, GLFW_CURSOR, GLFW_CURSOR_DISABLED);
 
 	// INITIALIZE GLAD + CHECK
 	if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
@@ -51,7 +79,7 @@ int main()
 	glEnable(GL_DEPTH_TEST);
 
 	// shader creation
-	Shader myShader("shaders/9/vs.glsl", "shaders/9/fs.glsl");
+	Shader myShader("shaders/10/vs.glsl", "shaders/10/fs.glsl");
 
 	// cube vertices
 	float vertices[] = {
@@ -208,6 +236,11 @@ int main()
 	// render loop
 	while (!glfwWindowShouldClose(window))
 	{
+		// per-frame time logic
+		float currentFrame = glfwGetTime();
+		deltaTime = currentFrame - lastFrame;
+		lastFrame = currentFrame;
+
 		// process any inputs
 		processInput(window);
 
@@ -223,20 +256,16 @@ int main()
 		glActiveTexture(GL_TEXTURE1);
 		glBindTexture(GL_TEXTURE_2D, texture1);
 
-		// use my shader program
+		// activate shader
 		myShader.use();
 
-		// view transform
-		glm::mat4 view = glm::mat4(1.0f);
-		view = glm::translate(view, glm::vec3(0.0f, 0.0f, -3.0f));
-
-		// projection transform
-		glm::mat4 projection = glm::mat4(1.0f);
-		projection = glm::perspective(glm::radians(45.0f), 800.0f / 600.0f, 0.1f, 100.0f);
-
-		// set transform uniforms
-		myShader.setMat4("view", view);
+		// pass projection matrix to shader
+		glm::mat4 projection = glm::perspective(glm::radians(camera.Zoom), (float)SCR_WIDTH / (float)SCR_HEIGHT, 0.1f, 100.0f);
 		myShader.setMat4("projection", projection);
+
+		// camera/view transformation
+		glm::mat4 view = camera.GetViewMatrix();
+		myShader.setMat4("view", view);
 
 		// bind/activate the VAO data we stored earlier (cube)
 		glBindVertexArray(VAO);
@@ -274,12 +303,51 @@ void framebuffer_size_callback(GLFWwindow* window, int width, int height)
 
 } // end of framebuffer_size_callback(...)
 
+// this function is called whenever the mouse moves
+void mouse_callback(GLFWwindow* window, double xposIn, double yposIn)
+{
+	float xpos = static_cast<float>(xposIn);
+	float ypos = static_cast<float>(yposIn);
+
+	if (firstMouse)
+	{
+		lastX = xpos;
+		lastY = ypos;
+		firstMouse = false;
+	}
+
+	float xoffset = xpos - lastX;
+	float yoffset = lastY - ypos;
+
+	lastX = xpos;
+	lastY = ypos;
+
+	camera.ProcessMouseMovement(xoffset, yoffset);
+} // end of mouse_callback()
+
+// this function is called when the mouse scroll-wheel changes
+void scroll_callback(GLFWwindow* window, double xoffset, double yoffset)
+{
+	camera.ProcessMouseMovement(xoffset, yoffset);
+} // end of scroll_callback()
 
 // this function is called whenever an input is observed
 void processInput(GLFWwindow* window)
 {
+	// window exit
 	if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
 		glfwSetWindowShouldClose(window, true);
+
+	// camera change
+	const float cameraSpeed = 2.5f * deltaTime;
+	if (glfwGetKey(window, GLFW_KEY_W) == GLFW_PRESS)
+		camera.ProcessKeyboard(FORWARD, deltaTime);
+	if (glfwGetKey(window, GLFW_KEY_S) == GLFW_PRESS)
+		camera.ProcessKeyboard(BACKWARD, deltaTime);
+	if (glfwGetKey(window, GLFW_KEY_A) == GLFW_PRESS)
+		camera.ProcessKeyboard(LEFT, deltaTime);
+	if (glfwGetKey(window, GLFW_KEY_D) == GLFW_PRESS)
+		camera.ProcessKeyboard(RIGHT, deltaTime);
 
 } // end of processInput(...)
 
