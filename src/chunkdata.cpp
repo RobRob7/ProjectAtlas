37652f45281b830fdf1abcffd71e67ae8889edac
RobRob7 <rjames11@nmsu.edu>
2025-11-30 00:31:15 -0700

    simple heightmap demo with grass, dirt, and stone

--- src/chunkdata.cpp
+++ src/chunkdata.cpp
@@ -7,13 +7,18 @@ ChunkData::ChunkData(int cx, int cz)
 	m_chunkZ = cz;
 	blocks_.fill(0);
 
+	int height = 8 + int(4 * sin(0.2 * (cx + m_chunkX * CHUNK_SIZE)) * cos(0.2 * (cz + m_chunkZ * CHUNK_SIZE)));
+
 	for (int x = 0; x < CHUNK_SIZE; ++x)
 	{
 		for (int z = 0; z < CHUNK_SIZE; ++z)
 		{
-			for (int y = 0; y < 4; ++y)
+			for (int y = 0; y < CHUNK_SIZE; ++y)
 			{
-				setBlocks(x, y, z, 1);
+				if (y > height) setBlocks(x, y, z, 0);
+				else if (y == height) setBlocks(x, y, z, 2);
+				else if (y > height - 3) setBlocks(x, y, z, 1);
+				else setBlocks(x, y, z, 3);
 			} // end for
 		} // end for
 	} // end for
