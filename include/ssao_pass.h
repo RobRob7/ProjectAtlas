a2b6d916b38b4379d477e55f5550ae5f9f133c32
RobRob7 <rjames11@nmsu.edu>
2026-02-24 18:02:26 -0700

    ssaopass UBO added

--- include/ssao_pass.h
+++ include/ssao_pass.h
@@ -1,10 +1,36 @@
 #ifndef SSAOPASS_H
 #define SSAOPASS_H
 
+#include "ubo_gl.h"
+
 #include <glm/glm.hpp>
 
 #include <memory>
 #include <array>
+
+inline constexpr int MAX_KERNEL_SIZE = 64;
+
+struct SSAOBlurUBO
+{
+	glm::vec2 u_texelSize;
+	glm::vec2 _pad0;
+};
+
+struct SSAOUBO
+{
+	glm::mat4 u_proj;
+	glm::mat4 u_invProj;
+
+	glm::vec2 u_noiseScale;
+	float u_radius;
+	float u_bias;
+
+	int32_t u_kernelSize;
+	float _pad0;
+	glm::vec2 _pad1;
+
+	glm::vec3 u_samples[MAX_KERNEL_SIZE];
+};
 
 class Shader;
 
@@ -41,10 +67,14 @@ private:
 	std::unique_ptr<Shader> ssaoShader_;
 	std::unique_ptr<Shader> blurShader_;
 
+	UBOGL uboBlur_{ UBOBinding::SSAOBlur };
+	SSAOBlurUBO ssaoBlurUBO_;
+	UBOGL uboSSAO_{ UBOBinding::SSAOPass };
+	SSAOUBO ssaoUBO_;
+
 	static constexpr int kNoiseSize_ = 4;
 	float radius_ = 5.0f;
 	float bias_ = 0.05f;
-	static constexpr int MAX_KERNEL_SIZE = 64;
 	int kernelSize_ = 64;
 	std::array<glm::vec3, MAX_KERNEL_SIZE> samples_{};
 
