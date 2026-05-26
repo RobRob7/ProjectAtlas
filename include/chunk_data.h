f4d254e775d59ee89646c3a6d65dee73e49871dd
RobRob7 <rjames11@nmsu.edu>
2026-05-25 18:08:36 -0600

    procedural cave generation + additional block types added

--- include/chunk_data.h
+++ include/chunk_data.h
@@ -20,19 +20,45 @@ public:
 	ChunkData(int cx, int cz);
 	~ChunkData();
 
-	BlockID getBlockID(int x, int y, int z) const;
-	void setBlockID(int x, int y, int z, BlockID id);
+	BlockID getBlockID(int x, int y, int z) const
+	{
+		return blocks_[x + CHUNK_SIZE * (z + CHUNK_SIZE * y)];
+	} // end of getBlockID()
 
-	const std::array<BlockID, CHUNK_SIZE * CHUNK_SIZE_Y* CHUNK_SIZE>& getBlocks() const;
+	void setBlockID(int x, int y, int z, BlockID id)
+	{
+		if (x < 0 || x >= CHUNK_SIZE ||
+			y < 0 || y >= CHUNK_SIZE_Y ||
+			z < 0 || z >= CHUNK_SIZE)
+		{
+			return;
+		}
 
-	void loadData(std::istream& in);
+		setBlocks(x, y, z, id);
+	} // end of setBlockID()
+
+	void loadData(std::istream& in)
+	{
+		in.read(
+			reinterpret_cast<char*>(blocks_.data()),
+			static_cast<std::streamsize>(blocks_.size() * sizeof(BlockID))
+		);
+	} // end of loadData()
+
+	const std::array<BlockID, CHUNK_SIZE* CHUNK_SIZE_Y* CHUNK_SIZE>& getBlocks() const
+	{
+		return blocks_;
+	} // end of getBlocks()
 
 private:
-	std::array<BlockID, CHUNK_SIZE * CHUNK_SIZE_Y* CHUNK_SIZE> blocks_;
+	std::array<BlockID, CHUNK_SIZE * CHUNK_SIZE_Y * CHUNK_SIZE> blocks_;
 	utils::NoiseMap heightMap_;
+	module::Perlin caveNoise_;
 private:
 	void setBlocks(int x, int y, int z, BlockID id);
 	void setupHeightMap(int cx, int cz);
+	void setupCaveNoise();
+	void carveCave(int x, int y, int z);
 	void placeTree(int x, int groundY, int z);
 };
 
